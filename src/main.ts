45a5553f4d08e4c68283eb093b3bd23615952eb1
srosa2000 <samuel@paralex.ai>
2025-06-02 15:05:44 -0400

    Refactor (Bootstrap): enhance logging during application startup using native nest logger and updating server start messages

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,11 @@ import { ConfigService } from '@nestjs/config';
 import { setupApp } from './config/app.setup';
 import { PermissionSeederService } from './features/auth/seeders/permission.seeder.service';
 import { RoleSeederService } from './features/auth/seeders/role.seeder.service';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
   const configService = app.get(ConfigService);
 
   setupApp(app);
@@ -26,9 +27,11 @@ async function bootstrap() {
   }
 
   const port = configService.get<number>('PORT');
-  await app.listen(port).then(() => {
-    console.log(`Server is running on port ${port}`);
-  });
+  const domain = configService.get<number>('DOMAIN');
+
+  await app.listen(port);
+  logger.log(`Server is running on http://${domain}:${port}`);
+  logger.log(`Swagger UI is available on http://${domain}:${port}/api/docs`);
 }
 
 bootstrap();
